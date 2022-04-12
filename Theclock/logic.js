bac5a32182c6ba6315e2f7e927c150b6b50e5af9
Camilo Andres Fuentes Susatama <camilo_andrech@hotmail.com>
2022-04-12 11:34:39 -0500

    Add files via upload

---
+++ Theclock/logic.js
@@ -0,0 +1,51 @@
+setInterval(setClock, 1000)
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock() {
+  const currentDate = new Date()
+  const secondsRatio = currentDate.getSeconds() / 60
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12
+  
+  setRotation(secondHand, secondsRatio)
+  setRotation(minuteHand, minutesRatio)
+  setRotation(hourHand, hoursRatio)
+
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty('--rotation', rotationRatio * 360)
+}
+
+setClock()
+
+
+
+/*setInterval(setClock, 1000);s
+
+const hourHand = document.querySelector('[data-hour-hand]')
+const minuteHand = document.querySelector('[data-minute-hand]')// Testing
+const secondHand = document.querySelector('[data-second-hand]')
+
+function setClock(){
+    const currentDate  = new Date();
+    const secondsRatio = currentDate.getSeconds() / 60
+    const minutesRatio = (secondsRatio+ secondsRatio.currentDate.getMinutes()) / 60
+    const hoursRatio = (minutesRatio+currentDate.getHours())/ 60
+    
+    setRotation(secondHand, secodsRatio)
+    setRotation(minuteHand, minutesRatio)
+    setRotation(hourHand, hoursRatio)
+}
+
+
+function setRotation(element,rotationRatio){
+
+    element.style.setProperty('--rotation', rotationRatio * 360)
+}
+ */
+
+
